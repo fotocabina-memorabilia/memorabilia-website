2d34db31f3f697b0ec64d8daa9cff3a72f6ba144
pinzonjulian <julianpinzoneslava@gmail.com>
2015-02-10 11:57:45 -0500

    HTML y CSS terminados
    Se terminó el proceso de HTML y CSS de la aplicación. Pendiente de
    revisión para empezar proceso de JS.

---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+$("#opciones-invitados input:radio").addClass("input_hidden"),$("#opciones-invitados label").click(function(){$(this).addClass("selected").siblings().removeClass("selected")}),$("#opciones-cliente input:radio").addClass("input_hidden"),$("#opciones-cliente label").click(function(){$(this).addClass("selected").siblings().removeClass("selected")});
