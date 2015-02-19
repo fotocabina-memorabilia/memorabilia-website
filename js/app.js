2f68d663d770f7615463dae0e74aa1f0e1deb22e
pinzonjulian <julianpinzoneslava@gmail.com>
2015-02-19 17:49:59 -0500

    2 nuevas features agregadas. Falta arreglar un par de bugs.
    Se agregó feature que muestra características del plan elegido tanto en
    la etapa de elección como en el resumen del plan.
    Hay dos bugs por arreglar:
    1. Los cuadros con la descripción de los planes son clickeables.
    Queremos que sean clickeables y cambien el costo del plan? Cómo trabaja
    eso en conjunto con el cálculo de los recordatorios?
    2. No he podido esconder el maldito div que muestra las características
    al final!!!!

--- js/app.js
+++ js/app.js
@@ -15,6 +15,12 @@ var $labels = $("label");
 
 var $outputPrecio = $("#precio-final");
 
+var $outputHoras = $("#output-horas");
+
+var $outputInvitados = $("#output-invitados");
+
+var $outputCliente = $("#output-cliente");
+
 //SERGIO
 var plan1 = 650000;
 var plan2 = 770000;
@@ -39,12 +45,15 @@ function actualizarRecordatorioInvitados(){
 
 	if($('#invitadosClasico').is(':checked')){
 		precioRecordatoriosInvitados = precioInvitadoClasico;
+		$outputInvitados.html("Recordatorios clásicos para invitados");
 		// console.log(precioRecordatoriosInvitados);
 	} else if($('#invitadosSobre').is(':checked')){
 		precioRecordatoriosInvitados = precioInvitadoSobre;
+		$outputInvitados.html("Fotos en sobre personalizados para invitados");
 		// console.log(precioRecordatoriosInvitados);
 	} else {
 		precioRecordatoriosInvitados = precioInvitadoPortarretratos;
+		$outputInvitados.html("Fotos con portarretratos para invitados");
 		// console.log(precioRecordatoriosInvitados);
 	}
 }
@@ -53,12 +62,15 @@ function actualizarRecordatorioCliente(){
 
 	if($('#clienteClasico').is(':checked')){
 		precioRecordatoriosCliente = precioClienteClasico;
+		$outputCliente.html("Recordatorios clásicos para ti");
 		// console.log(precioRecordatoriosCliente);
 	} else if($('#clienteCuadro').is(':checked')){
 		precioRecordatoriosCliente = precioClienteCuadro;
+		$outputCliente.html("Marco con todas las fotos colgadas");
 		// console.log(precioRecordatoriosCliente);
 	} else {
 		precioRecordatoriosCliente = precioClienteAlbum;
+		$outputCliente.html("Álbum fotográfico con firmas de los asistentes");
 		// console.log(precioRecordatoriosCliente);
 	}
 }
@@ -66,7 +78,10 @@ function actualizarRecordatorioCliente(){
 function actualizarPrecioFinal(precioBaseP, precioRecordatoriosInvitadosP, precioRecordatoriosClienteP, cantidadDeInvitadosP){
 	if(isNaN(cantidadDeInvitadosP) || cantidadDeInvitadosP<=0 || cantidadDeInvitadosP.length === 0){
 		$outputPrecio.html("Ingresa la cantidad de invitados para obtener el valor.");
+		
+
 	} else {
+		
 	precioFinal = precioBaseP + precioRecordatoriosInvitadosP*cantidadDeInvitadosP + precioRecordatoriosClienteP;
 	// console.log(precioFinal);
 
@@ -85,6 +100,7 @@ function actualizarPrecioFinal(precioBaseP, precioRecordatoriosInvitadosP, preci
 // ====== End --> Este código formatea números normales a moneda
 
 	$outputPrecio.html("El precio de tu plan es: " + precioFormateado + " COP.");
+
 }
 }
 
@@ -109,32 +125,54 @@ $cantidadDeInvitados.keyup(function(){
 			$(".span-planes").show();
 			// $("#tooltip-mobile").show();
 			$outputPrecio.html("Ingresa la cantidad de invitados para obtener el valor.");
+			$('#planSugerido1').prop('checked', false);
+			$('#planSugerido2').prop('checked', false);
+			$('#planSugerido3').prop('checked', false);
+
+			$("#output-caracteristicas").hide();
+			
 		} 
 		else if (cantidadDeInvitados>170) {
 
 			$(".span-planes").hide();
-			// $("#tooltip-mobile").hide();
 			$("#opciones").hide();
 			$("#costo-final-del-plan").hide();
+			$("#plan-diferente").hide();
+			$("#reserva").hide();
+			$("#plan-sugerido").hide();
 			$("#plan-especial").show();
+
+			$("#output-caracteristicas").hide();
 		}
 		else {
 			
 			$(".span-planes").hide();
-			// $("#tooltip-mobile").hide();
 			$("#opciones").show();
 			$("#plan-especial").hide();
 			$("#costo-final-del-plan").show();
+			$("#plan-sugerido").show();
+
+			$("#plan-diferente").show();
+			$("#reserva").show();
+			$("#output-caracteristicas").show();
+
+			$("#output-caracteristicas").show();
 
 			if (cantidadDeInvitados > 0 && cantidadDeInvitados <= 70){
 				precioBase = plan1;
+				$('#planSugerido1').prop('checked', true);
+				$outputHoras.html("2 horas");
+
 			} 
 			else if(cantidadDeInvitados > 70 && cantidadDeInvitados <= 119){
 				precioBase = plan2;
-
+				$('#planSugerido2').prop('checked', true);
+				$outputHoras.html("3 horas");
 			} 
 			else {
 				precioBase = plan3;
+				$('#planSugerido3').prop('checked', true);
+				$outputHoras.html("4 horas");
 
 			}
 			actualizarPrecioFinal(precioBase, precioRecordatoriosInvitados, precioRecordatoriosCliente, parseInt($cantidadDeInvitados.val()));
@@ -148,6 +186,7 @@ $labels.click(function(){
 	
 	setTimeout(actualizarRecordatorioInvitados, 200);
 	setTimeout(actualizarRecordatorioCliente, 200);
+
 	setTimeout(function(){
 		actualizarPrecioFinal(precioBase, precioRecordatoriosInvitados, precioRecordatoriosCliente, parseInt($cantidadDeInvitados.val()));
 	}, 200);
